6d57eedb5164ce8a80c95f99b1565bdfb86fc960
Éverton M. Vieira <everton.muvi@gmail.com>
2022-04-04 09:10:27 -0300

    prototype development

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use std::error::Error;
 
-mod clip;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let args = clip::parse();
-    let base = args
-        .value_of("base")
-        .expect("You must inform the base URL connection string.");
-    if let Some(dir) = args.value_of("export-to-csv") {
-        datx::export_to_csv(base, dir).await?;
-    }
-    Ok(())
+fn main() {
+    println!("Datx (Data Transform Toolbox) is a library and a command program that features a toolbox with a series of data transformation functionalities.")
 }
 
 
