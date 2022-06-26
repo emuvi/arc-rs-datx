48e76c4ad0563afe9f3ebbf858a9f4c37c23f67e
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-26 14:19:15 -0300

    implements the new look and zone plan on the sail

--- src/sail.rs
+++ src/sail.rs
@@ -7,10 +7,11 @@ impl Datx {
   pub fn start(self) -> RubxResult<()> {
     let mut retrieved: Vec<Crude> = Vec::new();
     for from in self.from {
-      let data: Vec<String> = match from.kind {
+      let data = match from.kind {
         Kind::Path(path) => read_path(path)?,
-        Kind::StdInPath => read_paths(read_stdin()?)?,
-        Kind::StdInBody => read_stdin()?,
+        Kind::StdInBody => read_stdin_body()?,
+        Kind::StdInAllPaths => read_paths(read_stdin_lines()?)?,
+        Kind::StdInLinePath => read_path(read_stdin_line()?)?,
       };
       retrieved.push(Crude {
         name: from.name,
@@ -20,7 +21,7 @@ impl Datx {
     println!("Retrieved: {:?}", retrieved);
     let mut processed: Vec<Cooked> = Vec::new();
     for pick in self.pick {
-      let data: Vec<String> = cook(&retrieved, &processed, pick.hunt, pick.zone)?;
+      let data = cook(&retrieved, &processed, pick.hunt, pick.look, pick.zone)?;
       processed.push(Cooked {
         name: pick.name,
         data,
@@ -35,39 +36,49 @@ impl Datx {
 #[derive(Debug)]
 struct Crude {
   name: String,
-  data: Vec<String>,
+  data: String,
 }
 
 /// Cooked is where the data that has been transformed is stored.
 #[derive(Debug)]
 struct Cooked {
   name: String,
-  data: Vec<String>,
+  data: String,
 }
 
-fn read_path(path: String) -> RubxResult<Vec<String>> {
-  use std::io::BufRead;
-  let file = std::fs::File::open(path)?;
-  let mut results = Vec::new();
-  for line in std::io::BufReader::new(file).lines() {
-    results.push(line?);
-  }
-  Ok(results)
+fn read_path(path: String) -> RubxResult<String> {
+  Ok(std::fs::read_to_string(path)?)
 }
 
-fn read_paths(paths: Vec<String>) -> RubxResult<Vec<String>> {
-  use std::io::BufRead;
-  let mut results = Vec::new();
+fn read_paths(paths: Vec<String>) -> RubxResult<String> {
+  let mut results = String::new();
   for path in paths {
-    let file = std::fs::File::open(path)?;
-    for line in std::io::BufReader::new(file).lines() {
-      results.push(line?);
-    }
+    let file = std::fs::read_to_string(path)?;
+    results.push_str(&file);
+    results.push('\n');
   }
   Ok(results)
 }
 
-fn read_stdin() -> RubxResult<Vec<String>> {
+fn read_stdin_body() -> RubxResult<String> {
+  use std::io::BufRead;
+  let mut results = String::new();
+  for line in std::io::stdin().lock().lines() {
+    results.push_str(line?.as_str());
+    results.push('\n');
+  }
+  Ok(results)
+}
+
+fn read_stdin_line() -> RubxResult<String> {
+  use std::io::BufRead;
+  for line in std::io::stdin().lock().lines() {
+    return Ok(line?.into());
+  }
+  Ok(String::default())
+}
+
+fn read_stdin_lines() -> RubxResult<Vec<String>> {
   use std::io::BufRead;
   let mut results = Vec::new();
   for line in std::io::stdin().lock().lines() {
@@ -80,13 +91,15 @@ fn cook<'a>(
   retrieved: &'a Vec<Crude>,
   processed: &'a Vec<Cooked>,
   hunt: Regex,
+  look: Look,
   zone: Zone,
-) -> RubxResult<Vec<String>> {
-  let mut results = vec![];
-  let collected = collect(retrieved, processed, zone);
+) -> RubxResult<String> {
+  let mut results = String::new();
+  let collected = collect(retrieved, processed, look, zone);
   for line in collected {
     for found in hunt.find_iter(line) {
-      results.push(found.as_str().into());
+      results.push_str(found.as_str());
+      results.push('\n');
     }
   }
   Ok(results)
@@ -95,28 +108,56 @@ fn cook<'a>(
 fn collect<'a>(
   retrieved: &'a Vec<Crude>,
   processed: &'a Vec<Cooked>,
+  look: Look,
   zone: Zone,
-) -> Vec<&'a String> {
-  let mut results: Vec<&String> = vec![];
+) -> Vec<&'a str> {
+  let mut results: Vec<&str> = vec![];
   match zone {
-    Zone::AllCrude => {
+    Zone::OnAllCrude => {
       for crude in retrieved {
-        for line in &crude.data {
-          results.push(line);
+        match look {
+          Look::OnWhole => results.push(crude.data.as_str()),
+          Look::OnLines => {
+            for line in crude.data.lines() {
+              results.push(line);
+            }
+          }
+        }
+      }
+    }
+    Zone::OnAllCooked => {
+      for cooked in processed {
+        match look {
+          Look::OnWhole => results.push(cooked.data.as_str()),
+          Look::OnLines => {
+            for line in cooked.data.lines() {
+              results.push(line);
+            }
+          }
         }
       }
     }
     Zone::OnCrude(name) => {
       if let Some(crude) = retrieved.iter().find(|crude| crude.name == name) {
-        for line in &crude.data {
-          results.push(line);
+        match look {
+          Look::OnWhole => results.push(crude.data.as_str()),
+          Look::OnLines => {
+            for line in crude.data.lines() {
+              results.push(line);
+            }
+          }
         }
       }
     }
     Zone::OnCooked(name) => {
-      if let Some(crude) = processed.iter().find(|crude| crude.name == name) {
-        for line in &crude.data {
-          results.push(line);
+      if let Some(cooked) = processed.iter().find(|cooked| cooked.name == name) {
+        match look {
+          Look::OnWhole => results.push(cooked.data.as_str()),
+          Look::OnLines => {
+            for line in cooked.data.lines() {
+              results.push(line);
+            }
+          }
         }
       }
     }
